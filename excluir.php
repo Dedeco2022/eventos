f553cc3ed16571cc1055d78680453d451a048567
Dedeco2022 <maria.2022315574@aluno.iffar.edu.br>
2024-11-22 20:27:56 -0300

    excluir.php

--- excluir.php
+++ excluir.php
@@ -6,4 +6,4 @@ require_once "conexao.php";
 $conexao = conectar();
 $sql = "DELETE FROM eventos WHERE id_evento = $id_evento";
 $retorno = executarSQL($conexao, $sql);
-echo json_encode($retorno);
+echo json_encode($retorno); 
