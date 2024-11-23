d5e132486e2bc1446b1aba5f9d03dee3e2613dae
Dedeco2022 <maria.2022315574@aluno.iffar.edu.br>
2024-11-22 23:57:01 -0300

    alterar

--- alterar.php
+++ alterar.php
@@ -4,13 +4,13 @@ require_once "conexao.php";
 $conexao = conectar();
 
 $usuario = json_decode(file_get_contents("php://input"));
-$sql = "UPDATE eventos SET
-        nome='$usuario->nome', 
-        lugar='$usuario->lugar',
-        categoria='$usuario->categoria',
-        dia='$usuario->dia'
-        WHERE id_evento=$usuario->id_evento";
 
-executarSQL($conexao, $sql); 
+$sql = "UPDATE livros SET
+        titulo='$usuario->titulo', 
+        categoria='$usuario->categoria',
+        autor='$usuario->autor',
+        WHERE id=$usuario->id";
+
+executarSQL($conexao, $sql);
 
 echo json_encode($usuario);
